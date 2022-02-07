d7ba1592cb155f2fe22194e45946692fa9a4b27e
franco sosa <gonza123e@gmail.com>
2022-02-07 05:40:39 -0300

    prueba

--- src/containers/intro/intro-style.js
+++ src/containers/intro/intro-style.js
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 export const IntroStyle = styled.div`
   height: 100vh;
-  background: #060606;
+  background: white;
 
   .App {
     text-align: center;
@@ -12,9 +12,34 @@ export const IntroStyle = styled.div`
     color: white;
     flex-direction: column;
     position: relative;
-    padding: 100px;
+    
   }
-
+  .nums-items{
+    padding-bottom: 15%;
+    color: rgba(0, 0, 0, 0.644);
+  }
+.description{
+  color:black;
+  display:flex;
+  flex-direction: column;
+    align-items: flex-start;
+    width:25vw ;
+    
+    
+    
+}
+.description-h4{
+  padding-bottom: 40px;
+  font-weight: bold;
+  padding-left: 30px
+}
+.container{
+  display:flex;
+  justify-content: space-between;
+  width: 90%;
+  height: 70%;
+  
+}
   .logo {
     width: 100px;
     margin-bottom: 30px;
@@ -31,10 +56,14 @@ export const IntroStyle = styled.div`
     display: flex;
     font-size: 12px;
   }
-
+.flexbox{
+  display:flex;
+}
   .slider-container {
-    width: 400px;
-    height: 200px;
+    width: 100vw;
+    height: 100vh;
+    position:relative;
+    z-index: 2;
   }
   .swiper-container {
     width: 100%;
@@ -42,45 +71,246 @@ export const IntroStyle = styled.div`
   }
   .swiper-container {
     width: 100%;
-    height: 100%;
+    height: 100vh;
+    display:flex;
   }
+  
+  &.activ{
+
+.swiper-slide{
+    animation-duration: 10s;
+    animation-name: slidein;
+   }
+   @keyframes slidein {
+    from {
+      .imag{
+        opacity: 10;
+      }
+      
+    }
+  
+    to {
+      .imag{
+        opacity: 0;
+      }
+    }
+  }
+
+
+
+  }
+  
   .swiper-slide {
     text-align: center;
     font-size: 18px;
-    background: black;
+    background: white;
     border: 1px solid white;
     display: flex;
     -webkit-box-pack: center;
     -ms-flex-pack: center;
-    -webkit-justify-content: center;
-    justify-content: center;
+    
+    
     -webkit-box-align: center;
     -ms-flex-align: center;
     -webkit-align-items: center;
     align-items: center;
   }
-
-  .header {
-    border: 1px solid white;
-    padding: 20px;
-    background: #060606;
-    color: white;
+  .swiper-title{
+    color:black;
+    
+  }
+  .menu{
+    width: 100%;
+    height: 100%;
+  }
+  .menu-options{
+    position: absolute;
+    z-index: 2;
+    color: black; 
+    font-size: 0vw;
+    top: 35vh;
+   
+    
+  }
+  .header{
+    color: black;
     display: flex;
+    font-weight: bold;
+    font-size:21px;
+    justify-content: space-between;
 
     &.active {
-      background: white;
-      color: black;
-      border: 1px solid black;
+      
+      
 
       .header-menu {
         border: 1px solid black;
+        border: solid 1px black !important;
+        animation-duration: 4s;
+        animation-name: header-menu;
+        color:white;
+        background-color: transparent;
+        
       }
+      @keyframes header-menu {
+        0% {
+          color:black;
+        }
+      
+        100% {
+          color:white;
+        }}
+      .mater{
+        animation-duration: 10s;
+        animation-name:mater;
+        color:white;
+      }
+      @keyframes mater {
+        0% {
+          color:black;
+        }
+      
+        100% {
+          color:white;
+        }}
+      .header-contenedor{
+       
+        display:flex;
+        justify-content: space-between;
+      }
+      
     }
   }
-
+  .menu-capa { 
+    &.activ {
+    animation-duration: 2s;
+    animation-name: capaMenu;
+    height: 100vh;
+    width: 100vw;
+    background-color: black;
+    position: absolute;
+    z-index: 6;
+    .menu-options{
+      animation-duration: 5s;
+      animation-name: menu-options;
+      position: absolute;
+      z-index: 2;
+      
+      font-size: 0vw;
+      top: 35vh;
+      
+      color: white;
+      font-size: 2vw;
+    }
+    
+    @keyframes menu-options {
+      0% {
+        color: black;
+        
+      }
+      25%{
+        color:black;
+        font-size: 1vw;
+        transform: translateY(50vh);
+      }
+    
+      100% {
+        color:white;
+        font-size: 2vw;
+        transform: translateY(0);
+      }}
+   
+      @keyframes capaMenu {
+        0% {
+        height: 100vh;
+        width: 1vw;
+        background-color: black;
+        position: absolute;
+        z-index: 6;
+        left: 99%;
+        }
+      
+        100% {
+          height: 100vh;
+        width: 100vw;
+        background-color: black;
+        position: absolute;
+        z-index: 6;
+        left: 0%;
+        }
+    }
+  }
   .header-menu {
-    border: 1px solid white;
-    padding: 20px;
+    height: 15vh;
+    padding: 25px 17px;
     margin-right: 12px;
+  }}
+  .opt{
+    font-size: 0px;
+  }
+  .sofa{
+    position: absolute;
+    z-index: 6;
+    width: 0vw;
+    transform: translateX(50vw);
+    
+  }
+ .options{
+      font-size: 3vw;
+    }
+  .cillection-options:hover{
+    .opt{
+      padding: 0 1vw 2vh;
+      color: rgb(192, 183, 183);
+    }
+    
+    .sofa{
+      position: absolute;
+      z-index: 6;
+      animation-duration: 2s;
+      animation-name: sofa;
+      width: 30vw
+    }
+    @keyframes sofa {
+      0% {
+        width: 10vw;
+      }
+    
+      100% {
+        width: 30vw;
+      }
+  }
+    .collection{
+      height: 10vh;
+      font-size: 3vw;
+      animation-duration: 2s;
+      animation-name: coll;
+      
+    }
+    @keyframes coll {
+      0% {
+        height: 0px;
+      }
+    
+      100% {
+        height: 10vh;
+      }
+  }
+    .opt{
+
+    animation-duration: 2s;
+    animation-name: opt;
+      font-size: 2vw;
+    }
+    @keyframes opt {
+      0% {
+        font-size: 0.5vw;
+      }
+    
+      100% {
+        font-size: 2vw;
+      }
+  }
+  
   }
 `;
--- src/containers/intro/intro.jsx
+++ src/containers/intro/intro.jsx
@@ -9,62 +9,137 @@ const Intro = () => {
   let mySwiper: any = null;
 
   const [active, setActive] = useState(false);
+  const [activ, setActiv] = useState(false);
 
   useEffect(() => {
     mySwiper = new Swiper(".swiper-container", {
       speed: 1000,
+
       direction: "vertical",
+      
+      
       navigation: {
+
+        type: "progressbar",
         nextEl: ".swiper-button-next",
         prevEl: ".swiper-button-prev",
       },
       pagination: {
         el: ".swiper-pagination",
-        type: "bullets",
+        type: "progressbar",
       },
     });
   }, []);
-
+console.log(activ);
   return (
     <IntroStyle>
-      <div className={`header ${active ? "active" : ""}`}>
+      <div className={`menu-capa ${active ? "activ" : ""}`}>
+      <div className="menu-options">
+        <div className="contenedor-options">
+<div className="cillection-options">  
+<h3 className="options">Collection</h3> 
+<div className="collection">
+  <div className="optF">
+<p  className="opt ">Fumiture</p>
+</div>
+<p className="opt">Lighting</p>
+<p className="opt">Acessories</p>
+<img className="sofa" src={require('../../assets/images/recursos/Sofá-blanco.png')} alt="" />
+</div>
+</div> 
+<h3 className="options">Design</h3>
+<h3 className="options">Craftmanship</h3>
+<h3 className="options">Ethics </h3>
+</div>
+</div>
+ </div>
+      <div className="container-header">
+      <div className={` header ${active ? "active" : ""}`}>
+        <h5 className="mater">mater</h5>
         <div className="header-menu" onClick={() => setActive(!active)}>
+          <img src='../../assets/images/images' alt="" />
           X
         </div>
       </div>
-
-      <div className="App">
-        <div className="logo">
-          <img src={logo} alt="" />
-        </div>
-        <div className="title">
-          <h1>FrontEnd Utils </h1>
-        </div>
-        <div className="container-buttons">
-          <p>--- boiler ReactJS ---</p>
-        </div>
-
-        <div className="slider-container">
-          <div className="swiper-container">
-            {/* <!-- Additional required wrapper --> */}
-            <div className="swiper-wrapper">
-              {/* <!-- Slides --> */}
-              <div className="swiper-slide">Slide 1</div>
-              <div className="swiper-slide">Slide 2</div>
-            </div>
-            {/* <!-- If we need pagination --> */}
-            <div className="swiper-pagination"></div>
-
-            {/* <!-- If we need scrollbar --> */}
-            <div className="swiper-scrollbar"></div>
-          </div>
-        </div>
       </div>
-      {/* <!-- If we need navigation buttons --> */}
-      <div className="swiper-button-prev"></div>
-      <div className="swiper-button-next"></div>
+<div className="App">
+
+
+<div className="container-buttons">
+  <p>--- boiler ReactJS ---</p>
+</div>
+
+<div className="slider-container" >
+  <div className="swiper-container">
+    {/* <!-- Additional required wrapper --> */}
+    <div className="swiper-wrapper">
+      {/* <!-- Slides --> */}
+
+
+      <div className="swiper-slide">
+
+        
+        <div className="container">
+          <div className="description " >
+            <p className="description-info">Michael W.Dreeben</p>
+            <h4 className="description-h4 ">
+              Dunes Anthrazite Black
+            </h4>
+            <p className="info-description">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus ipsa incidunt iste vel eius inventore ut sint est exercitationem aliquid molestiae
+            </p>
+          </div>
+          
+        </div><img  className="imag" src={require('../../assets/images/recursos/Mesa-negra.png')} alt="Meza negra" />
+
+      </div>
+      <div className="swiper-slide">
+        <div className="container">
+          <div className="description">
+            <h4 className="description-h4">
+              Dunes Anthrazite Black
+            </h4>
+            <p className="info-description">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus ipsa incidunt iste vel eius inventore ut sint est exercitationem aliquid molestiae
+            </p>
+          </div>
+          
+
+        </div>
+<img src={require('../../assets/images/recursos/Silla-negra.png')} alt="" />
+
+      </div>
+    </div>
+    {/* <!-- If we need pagination --> */}
+    <div className="swiper-pagination">
+
+    </div>
+    <div className="boton">
+      <div className="boton-content">
+        <h2>Product Detatails</h2>
+      </div>
+    </div>
+
+    {/* <!-- If we need scrollbar --> */}
+    <div className="swiper-scrollbar"></div>
+  </div>
+</div>
+</div>
+      
+      {/*
+       <!-- <div className="bottom"></div>If we need navigation buttons --> */}
+       
+       <div className="botons">
+       <div className="swiper-button-prev">
+         <p className="nums-items">1</p>
+       </div>
+<div className="swiper-button-next">
+  <p className="nums-items">2</p>
+</div>
+</div>
     </IntroStyle>
   );
+
 };
 
 export default Intro;
